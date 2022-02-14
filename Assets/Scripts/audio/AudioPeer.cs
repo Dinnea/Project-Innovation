df115a4c16f378c9ba82b375e7befa4f14f0bd66
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2022-02-14 11:27:52 +0100

    detect audio from audio source

---
+++ Assets/Scripts/audio/AudioPeer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    AudioSource audioSource;
+    public float[] samples = new float[512];
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
