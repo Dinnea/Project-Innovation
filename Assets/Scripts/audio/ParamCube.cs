b1808e331eda601f75a3b1e1a9e1aec0c26840c5
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2022-02-14 12:50:26 +0100

    buffers

--- Assets/Scripts/audio/ParamCube.cs
+++ Assets/Scripts/audio/ParamCube.cs
@@ -6,11 +6,21 @@ public class ParamCube : MonoBehaviour
 {
     public int band;
     public float startScale, scaleMultiplier;
-
+    public bool useBuffer;
     private void Update()
     {
-        transform.localScale = new Vector3( transform.localScale.x, 
-                                            (AudioPeer.frequencyBand[band] * scaleMultiplier) + startScale, 
+        if (useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x,
+                                            (AudioPeer.bandBuffer[band] * scaleMultiplier) + startScale,
                                             transform.localScale.z);
+        }
+        else 
+        {
+            transform.localScale = new Vector3(transform.localScale.x,
+                                            (AudioPeer.frequencyBand[band] * scaleMultiplier) + startScale,
+                                            transform.localScale.z);
+        }
+        
     }
 }
