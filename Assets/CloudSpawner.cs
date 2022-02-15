5d82d8da26e25f8f96b158eab347740e7590d751
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2022-02-15 16:20:21 +0100

    cloud spawner prefab

--- Assets/CloudSpawner.cs
+++ Assets/CloudSpawner.cs
@@ -6,11 +6,11 @@ public class CloudSpawner : MonoBehaviour
 {
     [SerializeField] GameObject cloud;
     [SerializeField] float upperX, lowerX, upperY, lowerY;
-    [SerializeField]int spawnNtimes = 10;
+    [SerializeField]int spawnNtimes = 100;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i<spawnNtimes; i++) SpawnCloud();
+        SpawnManyClouds(spawnNtimes);
     }
 
     // Update is called once per frame
@@ -23,5 +23,10 @@ public class CloudSpawner : MonoBehaviour
     {
         Vector3 position = new Vector3(Random.Range(lowerX, upperX), Random.Range(lowerY, upperY));
         Instantiate(cloud, position, Quaternion.identity);
+    }
+
+    public void SpawnManyClouds(int number)
+    {
+        for (int i = 0; i < number; i++) SpawnCloud();
     }
 }
