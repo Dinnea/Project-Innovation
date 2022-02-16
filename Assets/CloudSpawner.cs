bfe87f557289429aaf10b8ecf8b5d9465768431c
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2022-02-16 11:45:22 +0100

    clouds spawn @X000 points

--- Assets/CloudSpawner.cs
+++ Assets/CloudSpawner.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class CloudSpawner : MonoBehaviour
 {
     [SerializeField] GameObject cloud;
-    [SerializeField] float upperX, lowerX, upperY, lowerY;
+    [SerializeField] float upperX, lowerX, upperY, lowerY, offsetY;
     [SerializeField]int spawnNtimes = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnManyClouds(spawnNtimes);
+        //SpawnManyClouds(spawnNtimes);
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     void SpawnCloud()
     {
-        Vector3 position = new Vector3(Random.Range(lowerX, upperX), Random.Range(lowerY, upperY));
+        Vector3 position = new Vector3(Random.Range(lowerX, upperX), this.transform.position.y+ Random.Range(lowerY, upperY)+offsetY);
         Instantiate(cloud, position, Quaternion.identity);
     }
 
--- Assets/Points.cs
+++ Assets/Points.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using TMPro;
 
 public class Points : MonoBehaviour
 {
+    public UnityEvent OnPointsGathered;
 
     [SerializeField]TextMeshProUGUI text;
     float points = -100;
+    bool haveCloudsSpawned = false;
 
-
+    private void Update()
+    {
+        if(points%1000 == 0 && points!=0)
+        {
+            if (!haveCloudsSpawned)
+            {
+                OnPointsGathered?.Invoke();
+                haveCloudsSpawned = true;
+            }
+           
+        }
+        else if(haveCloudsSpawned == true)
+        {
+            haveCloudsSpawned = false;
+        }
+    }
     public void AddPoints(float pPoints)
     {
         points += pPoints;
@@ -20,4 +38,5 @@ public class Points : MonoBehaviour
     {
         text.text = points.ToString();
     }
+
 }
