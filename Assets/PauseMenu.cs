ac910642b517fe162452d977eadbc6f3a6cac567
Julian Riel <495622@student.saxion.nl>
2022-02-28 15:13:00 +0100

    Pause Ui Animations
    very swag

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -8,20 +8,36 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] GameObject playButton;
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject pauseButton;
-    [SerializeField] GameObject settingsMenu;
     [SerializeField] GameObject countdown;
+    
+
+    
+    
     public void PauseGame()
     {
         Time.timeScale = 0;
+        pauseButton.SetActive(false);
+        playButton.SetActive(true);
     }
+
+    /*public void PlaySlideIn()
+    {
+        animator.SetBool("SlideIn",true);
+    }
+
+    public void PlaySlideOut()
+    {
+        animator.SetBool("SlideIn",false);
+    }*/
 
     public void Continue()
     {
         playButton.SetActive(false);
         pauseButton.SetActive(true);
         pauseMenu.SetActive(false);
-        settingsMenu.SetActive(false);
+        Time.timeScale = 1;
         countdown.SetActive(true);
+        
     }
     
     public void ExitGameScene()
