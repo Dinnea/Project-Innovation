acbbdac4137f8d49428c33266f0f7170a86bbc95
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2022-03-01 11:48:39 +0100

    tutorial complete

---
+++ Assets/2D Assets/parallax/Parallax.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    float length, startPosition;
+    [SerializeField]GameObject cam;
+    [SerializeField] float parallaxEffect;
+
+    private void Start()
+    {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float distance = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+        if (temp > startPosition + length) startPosition += length;
+        else if (temp < startPosition - length) startPosition -= length;
+    }
+
+}
