80c44484587b8b81f931bd6845c1d4e3c20b4681
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2022-03-02 11:19:38 +0100

    Parallax implemnted
    -choice between horizontal and vertical parallax
    -Background animation
    -cloud and tree layers

--- Assets/2D Assets/parallax/Parallax.cs
+++ Assets/2D Assets/parallax/Parallax.cs
@@ -4,23 +4,53 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    float length, startPosition;
+    float size, startPosition;
     [SerializeField]GameObject cam;
-    [SerializeField] float parallaxEffect;
+    [SerializeField][Range(0f, 1f)] float parallaxEffect;
+    [SerializeField] bool horizontal, vertical;
 
     private void Start()
     {
-        startPosition = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        if (horizontal)
+        {
+            startPosition = transform.position.x;
+            size = GetComponent<SpriteRenderer>().bounds.size.x;
+        }
+        else if (vertical)
+        {
+            startPosition = transform.position.y;
+            size = GetComponent<SpriteRenderer>().bounds.size.y;
+        }
+       
     }
 
     private void FixedUpdate()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float distance = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
-        if (temp > startPosition + length) startPosition += length;
-        else if (temp < startPosition - length) startPosition -= length;
+        float temp;
+        float distance;
+
+        if (horizontal)
+        {
+            temp = (cam.transform.position.x * (1 - parallaxEffect));
+            distance = (cam.transform.position.x * parallaxEffect);
+            transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+        }
+        else if (vertical)
+        {
+            temp = (cam.transform.position.y * (1 - parallaxEffect));
+            distance = (cam.transform.position.y * parallaxEffect);
+            transform.position = new Vector3(transform.position.x, startPosition + distance, transform.position.z);
+        }
+        else
+        {
+            temp = 0;
+            distance = 0;
+            Debug.Log("No parallax direction selected.");
+        }
+
+
+        if (temp > startPosition + size) startPosition += size;
+        else if (temp < startPosition - size) startPosition -= size;
     }
 
 }
